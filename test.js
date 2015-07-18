4578c9b79af59fc3b25a7c83c4e00be03f44f448
1000ch <shogo.sensui@gmail.com>
2015-07-18 21:39:01 +0900

    add tests

--- test.js
+++ test.js
@@ -4,7 +4,21 @@ var assert    = require('power-assert');
 var readChunk = require('read-chunk');
 var isSketch  = require('./');
 
-it('should detect Sketch file from Buffer', function() {
-  assert(isSketch(readChunk.sync('blank.sketch', 0, 4)));
-  assert(isSketch(readChunk.sync('image.sketch', 0, 4)));
+describe('isSketch', function() {
+  it('should detect Sketch file from Buffer', function() {
+    assert(isSketch(readChunk.sync('blank.sketch', 0, 4)));
+    assert(isSketch(readChunk.sync('image.sketch', 0, 4)));
+  });
+
+  it('should return false from gif Buffer', function() {
+    assert(!isSketch(readChunk.sync('image.gif', 0, 4)));
+  });
+
+  it('should return false from jpg Buffer', function() {
+    assert(!isSketch(readChunk.sync('image.jpg', 0, 4)));
+  });
+
+  it('should return false from png Buffer', function() {
+    assert(!isSketch(readChunk.sync('image.png', 0, 4)));
+  });
 });
