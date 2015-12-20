12ade53c95cf56f2fae66bf194f27b0b8f19426d
1000ch <shogo.sensui@gmail.com>
2015-12-20 15:30:58 +0900

    remove t.end()

--- test/index.js
+++ test/index.js
@@ -6,20 +6,16 @@ const isSQlite  = require('../');
 
 test('should detect SQLite file from Buffer', t => {
   t.ok(isSQlite(readChunk.sync('fixture.sqlite', 0, 4)));
-  t.end();
 });
 
 test('should return false from gif Buffer', t => {
   t.ok(!isSQlite(readChunk.sync('fixture.gif', 0, 4)));
-  t.end();
 });
 
 test('should return false from jpg Buffer', t => {
   t.ok(!isSQlite(readChunk.sync('fixture.jpg', 0, 4)));
-  t.end();
 });
 
 test('should return false from png Buffer', t => {
   t.ok(!isSQlite(readChunk.sync('fixture.png', 0, 4)));
-  t.end();
 });
