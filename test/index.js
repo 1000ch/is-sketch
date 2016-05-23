6e0c6c982b1baf60b47e0672aaa811577a456c34
1000ch <shogo.sensui@gmail.com>
2016-05-23 09:12:14 +0900

    Fix tests

--- test/index.js
+++ test/index.js
@@ -1,21 +1,21 @@
 'use strict';
 
-const test      = require('ava');
+const test = require('ava');
 const readChunk = require('read-chunk');
-const isSQlite  = require('../');
+const isSQlite = require('../');
 
 test('should detect SQLite file from Buffer', t => {
-  t.ok(isSQlite(readChunk.sync('fixture.sqlite', 0, 4)));
+  t.true(isSQlite(readChunk.sync('fixture.sqlite', 0, 4)));
 });
 
 test('should return false from gif Buffer', t => {
-  t.ok(!isSQlite(readChunk.sync('fixture.gif', 0, 4)));
+  t.true(!isSQlite(readChunk.sync('fixture.gif', 0, 4)));
 });
 
 test('should return false from jpg Buffer', t => {
-  t.ok(!isSQlite(readChunk.sync('fixture.jpg', 0, 4)));
+  t.true(!isSQlite(readChunk.sync('fixture.jpg', 0, 4)));
 });
 
 test('should return false from png Buffer', t => {
-  t.ok(!isSQlite(readChunk.sync('fixture.png', 0, 4)));
+  t.true(!isSQlite(readChunk.sync('fixture.png', 0, 4)));
 });
